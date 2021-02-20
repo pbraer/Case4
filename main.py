daf77f4144fb50f7d8bcb9bd681143750f30a809
darya752 <73088666+darya752@users.noreply.github.com>
2021-02-20 07:44:05 +0300

    st

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print('Hello')
-print('Hi')
-print(':)')
+# Case-study #4
+# Developers:   Braer P. (%),
+#               Kokorina D. (%),
+#               Novoselov V. (%)
+from textblob import TextBlob
+t = input('Введите текст:')
+txt = TextBlob(t)
+sent = TextBlob.sentences(t)
+print(sent)
+import nltk
+nltk.download('punkt')
+
+
