c0b3a82b1fe201e9602af0c4a1398e36dad25e1b
pbraer <73088589+pbraer@users.noreply.github.com>
2021-02-20 11:36:09 +0700

    Step 1

--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 # Developers:   Braer P. (%),
 #               Kokorina D. (%),
 #               Novoselov V. (%)
+from textblob import TextBlob
+t = input('Введите текст:')
+txt = TextBlob(t)
+sent = TextBlob.sentences(t)
+print(sent)
