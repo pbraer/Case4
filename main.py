2ab3b45b08bbd3623bfbe9f237ec373dce572014
novoselovvasily <novoselov020@mail.ru>
2021-02-20 11:51:06 +0700

    nextstep

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print('Hello')
-print('Hi2')
+# Case-study #4
+# Developers:   Braer P. (%),
+#               Kokorina D. (%),
+#               Novoselov V. (%)
+from textblob import TextBlob
+t = input('Введите текст:')
+txt = TextBlob(t)
+print(len((txt.sentences)))
