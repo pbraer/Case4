2e608da49686a66e915d62eacc6f2944819b9a5e
darya752 <73088666+darya752@users.noreply.github.com>
2021-02-21 12:00:13 +0300

    st6

--- main.py
+++ main.py
@@ -4,16 +4,17 @@
 #               Novoselov V. (%)
 from textblob import TextBlob
 from translate import Translator
-import nltk
 t = input('Введите текст:')
 txt = TextBlob(t)
 vowel = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я', 'a', 'e', 'i', 'u', 'y', 'o']
 n = 0
-t2 = t.translate()
+translator = Translator(from_lang="russian", to_lang="english")
+t1 = translator.translate(t)
+t2 = t1.lower()
 print('Предложений:',len((txt.sentences)))
 print('Слов:',len((txt.words)))
-print('Средняя длина предложения в словах:',(len((txt.words))/len((txt.sentences))))
-print('Объективность:',(1 - txt.sentiment[1]),'%')
+print('Средняя длина предложения в словах:', (len((txt.words))/len((txt.sentences))))
+print('Объективность:',(1 - txt.sentiment[1]), '%')
 for i in range(len(t)):
     if t[i] in vowel:
         n += 1
