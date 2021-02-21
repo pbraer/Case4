cff598472848eb3cc0cb76c983c3fbb879ea7a18
darya752 <73088666+darya752@users.noreply.github.com>
2021-02-21 10:57:09 +0300

    st3

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ from textblob import TextBlob
 import nltk
 t = input('Введите текст:')
 txt = TextBlob(t)
+vowel = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я', 'a', 'e', 'i', 'u', 'y', 'o']
+n = 0
 print('Предложений:',len((txt.sentences)))
 print('Слов:',len((txt.words)))
 print('Средняя длина предложения в словах:',(len((txt.words))/len((txt.sentences))))
-FRE = 206.835 - (1.3 * ASL) - (60.1 * ASW)
-print('Индекс удобочитаемости Флеша:',FRE)
 print('Объективность:',(1 - txt.sentiment[1]),'%')
+for i in range(len(t)):
+    if t[i] in vowel:
+        n += 1
+print('Слогов:', n)
