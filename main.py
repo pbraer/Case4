9619dd8164e6fec67dae113be3fac377c97c2b9c
darya752 <73088666+darya752@users.noreply.github.com>
2021-02-21 18:39:05 +0300

    st10

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ for v in t:
         if v == let:
             sl = sl + 1
 
-ASL = (len((txt.words))/len((txt.sentences)))
-ASW = sl / len((txt.words))
-print('Предложений:',len((txt.sentences)))
-print('Слов:',len((txt.words)))
+ASL = (len(txt.words)/len(txt.sentences))
+ASW = sl / len(txt.words)
+print('Предложений:',len(txt.sentences))
+print('Слов:',len(txt.words))
 print('Средняя длина предложения в словах:', ASL)
 print('Слогов:', sl)
 print('Средняя длина слова в слогах:', ASW)
@@ -32,12 +32,15 @@ if lang == "ru":
     print('Объективность:',round(((1 - txt2.sentiment[1])*100),1), '%')
 else:
     print('Объективность:',round(((1 - txt.sentiment[1])*100),1), '%')
-analysisPol = TextBlob(t2).polarity
-if analysisPol == 0:
+if lang == "ru":
+    analysisPol = TextBlob(t1).polarity
+else:
+    analysisPol = TextBlob(t).polarity
+if -0.33 <= analysisPol <= 0.33:
     print('Тональность текста: нейтральный')
-elif analysisPol < 0:
+elif analysisPol < -0.33:
     print('Тональность текста: отрицательный')
-elif analysisPol > 0:
+elif analysisPol > 0.33:
     print('Тональность текста: положительный')
 if lang == "ru":
     fre=206.835-(1.3 * ASL)-(60.1*ASW)
@@ -51,7 +54,7 @@ if lang == "ru":
     elif fre < 25:
         print('Текст трудно читается (для выпускников ВУЗов).')
 else:
-    fre = 206.835-1.015 * (len((txt.words)) / len((txt.sentences)-(84.6*l_word)))
+    fre = 206.835-(1.015 * ASL)-(84.6 * ASW)
     print("Индекс удобочитаемости Флеша:", fre)
     if fre > 80:
         print('Текст очень легко читается (для младших школьников).')
@@ -61,4 +64,5 @@ else:
         print('Текст немного трудно читать (для студентов).')
     elif fre < 25:
         print('Текст трудно читается (для выпускников ВУЗов).')
+
 
