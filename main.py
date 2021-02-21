a70944c84f136bffc8751219b3ddf4b10cf43dfc
pbraer <73088589+pbraer@users.noreply.github.com>
2021-02-21 15:53:18 +0700

    changed transl

--- main.py
+++ main.py
@@ -3,12 +3,29 @@
 #               Kokorina D. (%),
 #               Novoselov V. (%)
 from textblob import TextBlob
+from translate import Translator
 import nltk
 t = input('Введите текст:')
 txt = TextBlob(t)
+vowel = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я', 'a', 'e', 'i', 'u', 'y', 'o']
+n = 0
+translator = Translator(from_lang="russian",to_lang="english")
+t2 = translator.translate(t)
+print(t2)
 print('Предложений:',len((txt.sentences)))
 print('Слов:',len((txt.words)))
 print('Средняя длина предложения в словах:',(len((txt.words))/len((txt.sentences))))
-FRE = 206.835 - (1.3 * ASL) - (60.1 * ASW)
-print('Индекс удобочитаемости Флеша:',FRE)
 print('Объективность:',(1 - txt.sentiment[1]),'%')
+for i in range(len(t)):
+    if t[i] in vowel:
+        n += 1
+print('Слогов:', n)
+l_word = n / len((txt.words))
+print('Средняя длина слова в слогах:', l_word)
+analysisPol = TextBlob(t2).polarity
+if analysisPol == 0:
+    print('Тональность текста: нейтральный')
+elif analysisPol < 0:
+    print('Тональность текста: отрицательный')
+elif analysisPol > 0:
+    print('Тональность текста: положительный')
